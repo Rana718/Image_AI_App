b87b921821ae1d5b4d6697db8c0db7076b782405
Rana718 <ranadolui718@gmail.com>
2024-11-06 00:30:32 +0530

    adding user get and post request

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,15 +1,54 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
 
 const userRoutes = new Hono();
+const prisma = new PrismaClient();
 
 
-userRoutes.get("/users", async (c) => {
-    const users = [
-        { id: 1, name: "John Doe" },
-        { id: 2, name: "Jane Doe" },
-    ];
+userRoutes.get("/user", async (c) => {
+    const email = c.req.query("email");
+    if (!email) {
+        return c.json({ error: "Email is required" }, 400);
+    }
 
-    return c.json(users);
+    try {
+        const user = await prisma.userList.findMany({
+            where: {
+                userEmail: email,
+            },
+        });
+
+        if (user.length === 0) {
+            return c.json({ message: "No user found with this email" }, 404);
+        }
+        return c.json(user);
+    } catch (e) {
+        console.error("Error fetching user data:", e);
+        return c.json({ error: "Internal server error" }, 500);
+    }
+})
+
+userRoutes.post("/user", async (c) => {
+    const { name, email } = await c.req.json();
+
+    if (!name || !email) {
+        return c.json({ error: "Name and email are required" }, 400);
+    }
+
+    try {
+        const user = await prisma.userList.create({
+            data: {
+                userName: name,
+                userEmail: email,
+                credits: 100
+            },
+        });
+
+        return c.json({ message: "User created successfully", user: user }, 201);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return c.json({ error: "Internal server error" }, 500);
+    }
 })
 
 
