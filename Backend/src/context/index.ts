a5a99a487244d8aec4b7d7db264f73021ca3cef7
Rana718 <ranadolui718@gmail.com>
2024-11-14 08:34:33 +0530

    add the context

---
+++ Backend/src/context/index.ts
@@ -0,0 +1,40 @@
+import cloudinary from '../../cloudinaryConfig';
+import { ImageTracking } from '../../types';
+
+
+const imageTracking: ImageTracking = {}
+
+interface addImageProps {
+    email: string,
+    publicId: string,
+    url: string,
+}
+
+export const addImageToTracking  = ({email, publicId, url}: addImageProps) => {
+    imageTracking[email] = {
+        url,
+        publicId,
+        uploadedAt: Date.now(),
+    };
+    startTimeOut(email);
+}
+
+
+const startTimeOut = (email: string) => {
+    setTimeout(async () =>{
+        if(imageTracking[email] && imageTracking[email].publicId) {
+            const timeDiff = Date.now() - imageTracking[email].uploadedAt;
+            if(timeDiff > 300000){
+                try{
+                    await cloudinary.uploader.destroy(imageTracking[email].publicId);
+                    delete imageTracking[email];
+                }catch(err) {
+                    console.log(err);
+                }
+            }
+        }
+    }, 300000);
+}
+
+
+export const getImageTracking = () => imageTracking;
--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Hono } from "hono";
 import userRoutes from "./userRoutes";
-import modelAddRoute from "./ModelAdd";
 import modelRoute from "../model";
-const routes = [userRoutes, modelAddRoute]
 
 const mainRoute = new Hono();
 
-routes.forEach((route) => {
-    mainRoute.route('/', route)
-})
+mainRoute.route('/', userRoutes)
 
 mainRoute.route('/ai', modelRoute)
 
